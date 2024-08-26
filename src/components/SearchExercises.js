af393b72cf9d78907979ad3031e4de0a7b6a88aa
takalkarsayali <sayali27.takalkar@gmail.com>
2024-08-26 14:31:55 +0530

    SearchExercises Updated

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -4,16 +4,44 @@ import { fetchData, exerciseOptions } from '../utils/fetchData';
 
 const SearchExercises = () => {
 
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState('');
+  const [exercises, setExercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/bodyPartList', exerciseOptions);
+      setBodyParts(['all', ...bodyPartsData]);
+    }
+
+    fetchExercisesData();
+  }, [])
+
 
   // handle Search Function
   const handleSearch = async () => {
-      // if search exists
-      if (search) {
-        const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);     //fetchData() is in utils folder ---- exerciseOptions are used to authorize the details 
+    // if search exists
+    if (search) {
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      //fetchData() is in utils folder ---- exerciseOptions are used to authorize the details 
+      // api: https://exercisedb.p.rapidapi.com/exercises  -->> gives all the exercises available 
+      // source of API: https://rapidapi.com/justin-WFnsXH_t6/api/exercisedb
 
-        console.log(exercisesData);
-      }
+      // flitering out data according to search
+      const searchedExercises = exercisesData.filter(
+        // add all the conditions that will find the exercises according to search of name,target,equipment or bodypart
+        (exercise) =>
+          exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search)
+      );
+
+      // clear search
+      setSearch('');
+      setExercises(searchedExercises);
+
+    }
   }
 
   return (
@@ -38,25 +66,29 @@ const SearchExercises = () => {
           }}
           height='76px'
           value={search}
-          onChange={(e) => {setSearch(e.target.value.toLocaleLowerCase())}}
+          onChange={(e) => { setSearch(e.target.value.toLocaleLowerCase()) }}
           type='text'
-          label="Search Exercises" 
-          />
-          <Button className='search-btn'
-            sx={{
-              bgcolor:"#ff2625",
-              color:"#fff",
-              textTransform:'none',
-              width:{ lg: '175px', xs:'80px'},
-              fontSize: { lg: '20px', xs:'14px'},
-              height:'56px',
-              position: "absolute",
-              right:'0'
-            }}
-            onClick={handleSearch}
-          >
-            Search
-          </Button>
+          label="Search Exercises"
+        />
+        <Button className='search-btn'
+          sx={{
+            bgcolor: "#ff2625",
+            color: "#fff",
+            textTransform: 'none',
+            width: { lg: '175px', xs: '80px' },
+            fontSize: { lg: '20px', xs: '14px' },
+            height: '56px',
+            position: "absolute",
+            right: '0'
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+      {/* Display Body Parts List */}
+      <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
+        <HorizontalScrollbar data={bodyParts} bodyParts setBodyPart={setBodyPart} bodyPart={bodyPart} />
       </Box>
     </Stack>
   )
